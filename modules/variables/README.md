4c38295002876f14303fba639491ed2a54541bca
akashreddyk-ikea <akash.kethireddy@inter.ikea.com>
2023-08-06 10:12:46 +0200

    Adding a new assignment about variables in python

---
+++ modules/variables/README.md
@@ -0,0 +1,52 @@
+
+### 1. Assignment Files
+
+Inside the cloned repository branch -> feature/learning-variables, you will find the following files:
+
+- `variables.py`: This is a Python file where you will write your code for the assignment.
+- `README.md`: The instructions and guidelines for the assignment (this file).
+
+Inorder to have the latest version of the repository in your local git, follow the below commands.
+
+        1. git pull
+    
+            gets the latest version of the repository.
+    
+       2. git checkout feature/learning-variables
+    
+            moving to the assignment branch to work on the assignment.
+    
+       3. Now you can start to work on this. Refer to the below comments for more details.
+
+### 2. Completing the Assignment
+
+Open the `variables.py` file in your preferred code editor. You will see a set of tasks with instructions, comments, and placeholders for your code.
+
+Your task is to complete the Python code for each task according to the instructions provided in the comments. The tasks will cover topics such as:
+
+- Basic syntax: Print statements, comments, and indentation.
+- Data types: Numbers, strings, lists, tuples, and dictionaries.
+- Variables: Assigning values and reassigning values to variables.
+- Operators: Arithmetic, comparison, logical, and assignment operators.
+
+Write your code in the corresponding sections, following the instructions carefully. Feel free to create new variables and experiment with the concepts as you work through the tasks.
+
+### 4. Testing Your Code
+
+After completing each task, you can run the `variables.py` file to test your code. Use the following command in the terminal:
+
+python module/varibles/variable.py
+
+
+### 5. Commit and Push
+
+Once you have finished all the tasks and verified your code, it's time to submit your work. Follow these steps:
+
+- Add your changes to the staging area:
+
+- Commit your changes with a descriptive message:
+
+- Push your changes to your GitHub repository:
+
+
+Good luck with the assignment, and happy coding!
---
+++ modules/variables/variables.py
@@ -0,0 +1,68 @@
+# Task 1: Basic Syntax
+# Print "Hello, World!" to the console
+
+# Task 2: Comments
+# Add a comment in this section to describe your favorite programming language.
+
+# Task 3: Indentation
+# Write a Python function called "multiply" that takes two arguments and returns their product.
+# Don't forget to use proper indentation!
+
+# Task 4: Numbers
+# Create two variables, num1 and num2, and assign them any numerical values of your choice.
+# Perform arithmetic operations (addition, subtraction, multiplication, division, and modulus) on these variables.
+# Print the results to the console.
+
+# Task 5: Strings
+# Create a variable called "message" and assign it a string containing your favorite quote.
+# Print the value of the "message" variable to the console.
+
+# Task 6: Lists
+# Create a list called "fruits" with the names of your favorite fruits (at least three).
+# Print the list to the console.
+
+# Task 7: Tuples
+# Create a tuple called "colors" with the names of different colors (at least five).
+# Print the tuple to the console.
+
+# Task 8: Dictionaries
+# Create a dictionary called "person" with the keys "name", "age", and "city".
+# Assign your name, age, and city to the corresponding keys.
+# Print the dictionary to the console.
+
+# Task 9: Variables Reassignment
+# Create a variable called "x" and assign it any numerical value of your choice.
+# Reassign the variable "x" to a different value.
+# Print the final value of "x" to the console.
+
+# Task 10: Arithmetic Operators
+# Write a Python function called "calculate_total" that takes three arguments: price, quantity, and tax_rate.
+# The function should calculate the total price by adding the product of price and quantity to the tax amount (tax_rate * price * quantity).
+# Return the total price from the function.
+# Test the function with some example values and print the result.
+
+# Task 11: Comparison Operators
+# Write a Python function called "is_even" that takes an integer as an argument and returns True if the number is even, False otherwise.
+# Test the function with some example values and print the results.
+
+# Task 12: Logical Operators
+# Write a Python function called "is_eligible" that takes two boolean arguments: age_over_18 and has_id.
+# The function should return True if both age_over_18 and has_id are True, otherwise return False.
+# Test the function with different combinations of values and print the results.
+
+# Task 13: Assignment Operators
+# Create a variable "counter" and set its initial value to 10.
+# Use assignment operators to perform the following operations:
+# - Increment the counter by 5
+# - Multiply the counter by 2
+# - Divide the counter by 3
+# Print the final value of the counter to the console.
+
+# Task 14: Bonus (Optional)
+# Explore Python's "input" function and create a simple program that takes the user's name as input and greets the user.
+# Print a personalized greeting message to the console.
+
+# Task 15: Bonus (Optional)
+# Create additional tasks to practice other Python concepts you've learned!
+
+# Your code for the assignment goes below this line:
